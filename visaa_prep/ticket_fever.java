9de62437ed2fc511424387a19cfb2fdeb9563e72
preethii04 <175088079+preethii04@users.noreply.github.com>
2024-11-17 22:07:21 +0530

    Update ticket_fever.java

--- visaa_prep/ticket_fever.java
+++ visaa_prep/ticket_fever.java
@@ -1,1 +1,22 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int count;
+            if(n>m){
+                count = n-m;
+            } else{
+                count =0;
+            }
+            System.out.println(count);
+        }
+        sc.close();
+    }
+}
