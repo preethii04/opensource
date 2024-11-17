64a67d21fc8ee035645b8f2909ddc0498fe82096
preethii04 <175088079+preethii04@users.noreply.github.com>
2024-11-17 21:48:39 +0530

    Create reverse array.java

---
+++ visaa_prep/reverse array.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int l =0;
+        int r = n-1;
+        while(l<r){
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+        for(int i=0;i<n;i++) {
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
