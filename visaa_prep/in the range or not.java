a93b4d685ad54f92b164623da967d3321a013329
preethii04 <175088079+preethii04@users.noreply.github.com>
2024-11-17 13:20:03 +0530

    Create in the range or not.java

---
+++ visaa_prep/in the range or not.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            int x = sc.nextInt();
+            if (x>= 67 && x<= 45000){
+                System.out.println("YES");
+            } else{
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+}
