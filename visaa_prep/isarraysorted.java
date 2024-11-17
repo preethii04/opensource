f6bdc7d9ad0b662356b654bef1e375305bf9e27a
preethii04 <175088079+preethii04@users.noreply.github.com>
2024-11-17 12:33:04 +0530

    Create isarraysorted.java

---
+++ visaa_prep/isarraysorted.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static boolean sort(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(sort(arr));
+        sc.close();
+    }
+}
